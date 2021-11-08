ccca660ff143f5e587525809a241519534689ff3
Lindrit Krasniqi <linndritks@gmail.com>
2021-11-08 10:12:37 +0100

    Create reset password link

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return env('APP_CLIENT_URL') . "/accounts/reset?token=$token&email=$user->email";
+        });
     }
 }
